6de47d5753dd3fe3f34e15d8f06fad036f46a6d9
yojik2901 <yojik2901@gmail.com>
2019-03-24 23:16:29 +0300

    #2 - Realization Flyweight and AbstractFactory and Singleton

---
+++ MyProgramIngeneringProject/IProductFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace MyProgramIngeneringProject
+{
+    public interface IProductFactory
+    {
+        IProduct CreateProduct(int id);
+    }
+}
---
+++ MyProgramIngeneringProject/ProductFactory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace MyProgramIngeneringProject
+{
+    public class ProductFactory: IProductFactory
+    {
+        #region Realization Singlon
+
+        private static IProductFactory _instance;
+
+        private ProductFactory() {}
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static IProductFactory GetInstance()
+        {
+            if(_instance == null)
+                _instance = new ProductFactory();
+
+            return _instance;
+        }
+
+        #endregion
+
+        #region Create product
+
+        private static Dictionary<int, IProduct> _products = new Dictionary<int, IProduct>();
+
+        public IProduct CreateProduct(int id)
+        {
+            IProduct product = null;
+
+            if (!_products.TryGetValue(id, out product))
+            {
+                switch (id)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                }
+
+                _products[id] = product;
+            }
+
+            return product;
+        }
+
+        #endregion
+    }
+}
